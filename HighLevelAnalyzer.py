a2dca3bdd1d43ee59991d495596534ef29b3cee5
Sebastian Herber <sebastian.herber@hbm.com>
2021-03-22 14:16:52 +0100

    initial commit IO Link Analyser

---
+++ HighLevelAnalyzer.py
@@ -0,0 +1,111 @@
+# High Level Analyzer
+# For more information and documentation, please go to https://support.saleae.com/extensions/high-level-analyzer-extensions
+
+from saleae.analyzers import HighLevelAnalyzer, AnalyzerFrame, StringSetting, NumberSetting, ChoicesSetting
+import IOLinkFrame
+
+type1_frames = {
+    #          (pd, od)
+    'Type_1_1': (2, 0),
+    'Type_1_2': (0, 2),
+    'Type_1_V (8 OD)': (0, 8),
+    'Type_1_V (32 OD)': (0, 32),
+}
+
+type2_frames = {
+    #      (pdout, od, pdin)
+    'Type_2_1': (0, 1, 1),
+    'Type_2_2': (0, 1, 2),
+    'Type_2_3': (1, 1, 0),
+    'Type_2_4': (2, 1, 0),
+    'Type_2_5': (1, 1, 1),
+    'Type_2_6': (2, 1, 2),
+    'Type_2_V': (0, 0, 0),
+}
+
+
+# High level analyzers must subclass the HighLevelAnalyzer class.
+class Hla(HighLevelAnalyzer):
+    # List of settings that a user can set for this High Level Analyzer.
+    type1_frame = ChoicesSetting(label = 'Type_1', choices = type1_frames.keys())
+    type2_frame = ChoicesSetting(label = 'Type_2', choices = type2_frames.keys())
+    pdout_len = NumberSetting(label='PDout length (only with Type_2_V)', min_value=0.0, max_value=32.0)
+    od_len = NumberSetting(label='OD length (only with Type_2_V)', min_value=0.0, max_value=32.0)
+    pdin_len = NumberSetting(label='PDin length (only with Type_2_V)', min_value=0.0, max_value=32.0)
+
+    # An optional list of types this analyzer produces, providing a way to customize the way frames are displayed in Logic 2.
+    result_types = {
+        'Type_0': {
+            'format': '{{data.Direction}} {{data.Channel}} {{data.Addr}} OD: {{data.OD}}'
+        },
+        'Type_1_1': {
+            'format': '{{data.Direction}} {{data.Channel}} {{data.Addr}} PD: {{data.PD}}'
+        },
+        'Type_1_2': {
+            'format': '{{data.Direction}} {{data.Channel}} {{data.Addr}} OD: {{data.OD}}'
+        },
+        'Type_1_V': {
+            'format': '{{data.Direction}} {{data.Channel}} {{data.Addr}} OD: {{data.OD}}'
+        },
+        'Type_2_1': {
+            'format': '{{data.Direction}} {{data.Channel}} {{data.Addr}} OD: {{data.OD}} PDin: {{data.PDin}}'
+        },
+        'Type_2_2': {
+            'format': '{{data.Direction}} {{data.Channel}} {{data.Addr}} OD: {{data.OD}} PDin: {{data.PDin}}'
+        },
+        'Type_2_3': {
+            'format': '{{data.Direction}} {{data.Channel}} {{data.Addr}} PDout: {{data.PDout}} OD: {{data.OD}}'
+        },
+        'Type_2_4': {
+            'format': '{{data.Direction}} {{data.Channel}} {{data.Addr}} PDout: {{data.PDout}} OD: {{data.OD}}'
+        },
+        'Type_2_5': {
+            'format': '{{data.Direction}} {{data.Channel}} {{data.Addr}} PDout: {{data.PDout}} OD: {{data.OD}} PDin: {{data.PDin}}'
+        },
+        'Type_2_6': {
+            'format': '{{data.Direction}} {{data.Channel}} {{data.Addr}} PDout: {{data.PDout}} OD: {{data.OD}} PDin: {{data.PDin}}'
+        },
+    }
+
+    def __init__(self):
+        self.process = self.parseByte()
+        self.process.send(None)
+        str = '{{data.Direction}} {{data.Channel}} {{data.Addr}}'
+        if self.pdout_len > 0:
+            str = str + ' PDout: {{data.PDout}}'
+        if self.od_len > 0:
+            str = str + ' OD: {{data.OD}}'
+        if self.pdin_len > 0:
+            str = str + ' PDin: {{data.PDin}}'
+        self.result_types['Type_2_V'] = {'format': str}
+        type2_frames['Type_2_V'] = (self.pdout_len, self.od_len, self.pdin_len)
+
+    def initFrame(self, frame0, frame1):
+        frametype = frame1.data["data"][0] >> 6
+        if frametype == 0:
+            return IOLinkFrame.IOLinkFrameType0(frame0, frame1)
+        if frametype == 1:
+            len = type1_frames[self.type1_frame]
+            return IOLinkFrame.IOLinkFrameType1(self.type1_frame[:8], len, frame0, frame1)
+        if frametype == 2:
+            len = type2_frames[self.type2_frame]
+            return IOLinkFrame.IOLinkFrameType2(self.type2_frame, len, frame0, frame1)
+        return None
+
+    def parseByte(self):
+        frame: AnalyzerFrame = yield None
+        while True:
+            pendingFrame = self.initFrame(frame, (yield None))
+            ret: AnalyzerFrame = None
+            while ret is None:
+                ret = pendingFrame.append((yield None))
+            frame = yield ret
+
+    def decode(self, frame: AnalyzerFrame):
+        if frame.type != 'data':
+            return
+
+        if "error" in frame.data:
+            return
+
+        return self.process.send(frame)
---
+++ IOLinkFrame.py
@@ -0,0 +1,149 @@
+# High Level Analyzer
+# For more information and documentation, please go to https://support.saleae.com/extensions/high-level-analyzer-extensions
+
+from saleae.analyzers import AnalyzerFrame
+
+
+chksum = (0x2d, 0x3c, 0x0c, 0x1d, 0x3f, 0x2e, 0x1e, 0x0f,
+          0x0f, 0x1e, 0x2e, 0x3f, 0x1d, 0x0c, 0x3c, 0x2d,
+          0x39, 0x28, 0x18, 0x09, 0x2b, 0x3a, 0x0a, 0x1b,
+          0x1b, 0x0a, 0x3a, 0x2b, 0x09, 0x18, 0x28, 0x39,
+          0x09, 0x18, 0x28, 0x39, 0x1b, 0x0a, 0x3a, 0x2b,
+          0x2b, 0x3a, 0x0a, 0x1b, 0x39, 0x28, 0x18, 0x09,
+          0x1d, 0x0c, 0x3c, 0x2d, 0x0f, 0x1e, 0x2e, 0x3f,
+          0x3f, 0x2e, 0x1e, 0x0f, 0x2d, 0x3c, 0x0c, 0x1d,
+          0x35, 0x24, 0x14, 0x05, 0x27, 0x36, 0x06, 0x17,
+          0x17, 0x06, 0x36, 0x27, 0x05, 0x14, 0x24, 0x35,
+          0x21, 0x30, 0x00, 0x11, 0x33, 0x22, 0x12, 0x03,
+          0x03, 0x12, 0x22, 0x33, 0x11, 0x00, 0x30, 0x21,
+          0x11, 0x00, 0x30, 0x21, 0x03, 0x12, 0x22, 0x33,
+          0x33, 0x22, 0x12, 0x03, 0x21, 0x30, 0x00, 0x11,
+          0x05, 0x14, 0x24, 0x35, 0x17, 0x06, 0x36, 0x27,
+          0x27, 0x36, 0x06, 0x17, 0x35, 0x24, 0x14, 0x05,
+          0x05, 0x14, 0x24, 0x35, 0x17, 0x06, 0x36, 0x27,
+          0x27, 0x36, 0x06, 0x17, 0x35, 0x24, 0x14, 0x05,
+          0x11, 0x00, 0x30, 0x21, 0x03, 0x12, 0x22, 0x33,
+          0x33, 0x22, 0x12, 0x03, 0x21, 0x30, 0x00, 0x11,
+          0x21, 0x30, 0x00, 0x11, 0x33, 0x22, 0x12, 0x03,
+          0x03, 0x12, 0x22, 0x33, 0x11, 0x00, 0x30, 0x21,
+          0x35, 0x24, 0x14, 0x05, 0x27, 0x36, 0x06, 0x17,
+          0x17, 0x06, 0x36, 0x27, 0x05, 0x14, 0x24, 0x35,
+          0x1d, 0x0c, 0x3c, 0x2d, 0x0f, 0x1e, 0x2e, 0x3f,
+          0x3f, 0x2e, 0x1e, 0x0f, 0x2d, 0x3c, 0x0c, 0x1d,
+          0x09, 0x18, 0x28, 0x39, 0x1b, 0x0a, 0x3a, 0x2b,
+          0x2b, 0x3a, 0x0a, 0x1b, 0x39, 0x28, 0x18, 0x09,
+          0x39, 0x28, 0x18, 0x09, 0x2b, 0x3a, 0x0a, 0x1b,
+          0x1b, 0x0a, 0x3a, 0x2b, 0x09, 0x18, 0x28, 0x39,
+          0x2d, 0x3c, 0x0c, 0x1d, 0x3f, 0x2e, 0x1e, 0x0f,
+          0x0f, 0x1e, 0x2e, 0x3f, 0x1d, 0x0c, 0x3c, 0x2d)
+
+
+class IOLinkFrame(AnalyzerFrame):
+
+    def __init__(self, type, framelength, first_frame, second_frame):
+        data = first_frame.data["data"][0]
+        super().__init__(type, first_frame.start_time, second_frame.end_time, {
+            'Direction': ('Write', 'Read')[(data & 0x80) != 0],
+            'Addr': bytes([data & 0x1F]),
+            'Channel': ('Process', 'Page', 'Diagnosis', 'ISDU')[(data >> 5) & 3]})
+        self.numOfBytes = 2
+        self.frameLength = framelength
+        data2 = second_frame.data["data"][0]
+        self.cktacc = data ^ (data2 & 0xC0)
+        self.cksacc = 0
+        self.cktksum = data2 & 0x3F
+
+    def append(self, frame):
+        self.numOfBytes = self.numOfBytes + 1
+        self.end_time = frame.end_time
+        if self.numOfBytes == self.frameLength:
+            self.data["valid"] = (frame.data["data"][0] & 0x40) == 0
+            self.data["event"] = (frame.data["data"][0] & 0x80) != 0
+            ckssum = frame.data["data"][0] & 0x3f
+            if (self.cktksum != chksum[self.cktacc]) and (ckssum != chksum[self.cksacc]):
+                self.error = "CKT, CKS"
+                self.data["error"] = self.error
+            elif chksum[self.cktacc] != self.cktksum:
+                self.error = "CKT"
+                self.data["error"] = self.error
+            elif chksum[self.cksacc] != ckssum:
+                self.error = "CKS"
+                self.data["error"] = self.error
+            return self
+
+    def printframe(self):
+        addr = self.data["Addr"][0]
+        print(self.data["direction"] + " Pageaddress " + hex(addr) + " ("+ str(addr) + "d)")
+
+
+class IOLinkFrameType0(IOLinkFrame):
+
+    def __init__(self, first_frame, second_frame):
+        super().__init__("Type_0", 4, first_frame, second_frame)
+
+    def append(self, frame):
+        data = frame.data["data"][0]
+        if self.numOfBytes == 2:
+            self.data["OD"] = "0x{:02x}".format(data)
+            if self.data['Direction'] == 'Write':
+                self.cktacc = self.cktacc ^ data
+            else:
+                self.cksacc = self.cksacc ^ data
+        return super().append(frame)
+
+    def printframe(self):
+        addr = self.data["Addr"][0]
+        print(self.data["Direction"] + " Pageaddress " + hex(addr) + " ("+ str(addr) + "d) Value: " + self.data["OD"][:4])
+
+
+class IOLinkFrameType1(IOLinkFrame):
+
+    def __init__(self, name, len, first_frame, second_frame):
+        super().__init__(name, sum(len) + 3, first_frame, second_frame)
+        self.pd_len = len[0]
+        self.od_len = len[1]
+        self.key = ("OD", "PD")[self.pd_len > 1]
+
+    def append(self, frame):
+        if self.numOfBytes < self.frameLength - 1:
+            data = frame.data["data"][0]
+            if self.data['Direction'] == 'Write':
+                self.cktacc = self.cktacc ^ data
+            else:
+                self.cksacc = self.cksacc ^ data
+            self.data[self.key] = self.data.get(self.key, '0x') + "{:02x}".format(data)
+        return super().append(frame)
+
+    def printframe(self):
+        addr = self.data["Addr"][0]
+        print(self.data["Direction"] + " Pageaddress " + hex(addr) + " ("+ str(addr) + "d) Value: " + self.data[self.key][:4])
+
+
+class IOLinkFrameType2(IOLinkFrame):
+
+    def __init__(self, name, len, first_frame, second_frame):
+        super().__init__(name, sum(len) + 3, first_frame, second_frame)
+        self.pdin_len = len[2]
+        self.pdout_len = len[0]
+        self.od_len = len[1]
+
+    def append(self, frame):
+        data = frame.data["data"][0]
+        if self.numOfBytes < (2 + self.pdout_len):
+            self.cktacc = self.cktacc ^ data
+            self.data['PDout'] = self.data.get('PDout', '0x') + "{:02x}".format(data)
+        elif self.numOfBytes < (2 + self.pdout_len + self.od_len):
+            if self.data['Direction'] == 'Write':
+                self.cktacc = self.cktacc ^ data
+            else:
+                self.cksacc = self.cksacc ^ data
+            self.data['OD'] = self.data.get('OD', '0x') + "{:02x}".format(data)
+        elif self.numOfBytes < (2 + self.pdout_len + self.od_len + self.pdin_len):
+            self.data['PDin'] = self.data.get('PDin', '0x') + "{:02x}".format(data)
+            self.cksacc = self.cksacc ^ data
+
+        return super().append(frame)
+
+    def printframe(self):
+        addr = self.data["Addr"][0]
+        print(self.data["Direction"] + " Pageaddress " + hex(addr) + " ("+ str(addr) + "d) Value: " + self.data["OD"][:4])

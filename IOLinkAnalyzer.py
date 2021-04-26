c52e9e3ff5152cbe20c32bee1b71356ce25ac3d1
Sebastian Herber <sebastian.herber@hbm.com>
2021-04-26 09:35:17 +0200

    added MIT license header

--- IOLinkAnalyzer.py
+++ IOLinkAnalyzer.py
@@ -1,3 +1,29 @@
+# SPDX-License-Identifier: MIT
+#
+# The MIT License (MIT)
+#
+# Copyright (c) <2021> Hottinger Brüel & Kjaer GmbH, Im Tiefen See 45, 64293 Darmstadt, Germany
+#
+# Permission is hereby granted, free of charge, to any person obtaining
+# a copy of this software and associated documentation files (the
+# "Software"), to deal in the Software without restriction, including
+# without limitation the rights to use, copy, modify, merge, publish,
+# distribute, sublicense, and/or sell copies of the Software, and to
+# permit persons to whom the Software is furnished to do so, subject to
+# the following conditions:
+#
+# The above copyright notice and this permission notice shall be
+# included in all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+# EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+# MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+# NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS
+# BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN
+# ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+# CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
 # High Level Analyzer
 # For more information and documentation, please go to https://support.saleae.com/extensions/high-level-analyzer-extensions
 
@@ -27,14 +53,14 @@ type2_frames = {
 
 # High level analyzers must subclass the HighLevelAnalyzer class.
 class IOLinkAnalyzer(HighLevelAnalyzer):
-    # List of settings that a user can set for this High Level Analyzer.
+    # Frame Subtype Settings
     type1_frame = ChoicesSetting(label = 'Type_1', choices = type1_frames.keys())
     type2_frame = ChoicesSetting(label = 'Type_2', choices = type2_frames.keys())
     pdout_len = NumberSetting(label='PDout length (only with Type_2_V)', min_value=0.0, max_value=32.0)
     od_len = NumberSetting(label='OD length (only with Type_2_V)', min_value=0.0, max_value=32.0)
     pdin_len = NumberSetting(label='PDin length (only with Type_2_V)', min_value=0.0, max_value=32.0)
 
-    # An optional list of types this analyzer produces, providing a way to customize the way frames are displayed in Logic 2.
+    # For each M-Sequence type a formatting string is provided. Type_2_V formatting is generated in __init__
     result_types = {
         'Type_0': {
             'format': '{{data.Direction}} {{data.Channel}} {{data.Addr}} OD: {{data.OD}}'
